0c7d83164bc422ae21762f3127f0e9d895e21a12
sergey123423213 <grinya.0895@gmail.com>
2025-03-17 19:58:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // api - start
+let importantPage = 1
+
+
 const getEventApi = async (keyword) => {
     if (keyword === "" || keyword === undefined || keyword === null){
         try {
-            const result = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=RKwTp5P44ztFFCbPCDFoxyCASf3hPfPS&size=20&page=1`).then((data) => {
+            const result = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=RKwTp5P44ztFFCbPCDFoxyCASf3hPfPS&size=20&page=${importantPage}`).then((data) => {
                 return data.json();
             });
             return result;
@@ -11,7 +14,7 @@ const getEventApi = async (keyword) => {
         }
     } else {
         try {
-            const result = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=RKwTp5P44ztFFCbPCDFoxyCASf3hPfPS&size=20&page=1&keyword=${keyword}`).then((data) => {
+            const result = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=RKwTp5P44ztFFCbPCDFoxyCASf3hPfPS&size=20&page=${importantPage}&keyword=${keyword}`).then((data) => {
                 return data.json();
             });
             return result;
@@ -25,6 +28,23 @@ getEventApi()
 getEventApi().then((data) => {
     createMarkup(data._embedded)
 });
+const pagination = document.querySelectorAll('.footer-pagination-page')
+pagination.forEach(function(element,index,array){
+    element.addEventListener('click',function(){
+        pagination.forEach(function(element){
+            if(element.classList.contains('active-page')){
+                element.classList.remove('active-page')
+            }
+        })
+        importantPage = Number(element.textContent)
+        getEventApi().then((data) => {
+            createMarkup(data._embedded)
+        });
+        element.classList.add('active-page')
+        console.log(importantPage)
+    })
+})
+
 // show card - start
 const list = document.querySelector(".main");
 let cards 
@@ -33,7 +53,6 @@ let cards
     const html = await arr.events.map((item) => {
         console.log(item)
         return `<div class="main-cards-card">
-                     <div class="main__style-div"></div>
                      <img class="main-cards-card-pic" src="${item.images[0].url}" alt="poster"/>
                      <h2 class="main-cards-card-title">${item.name}</h2>
 <span class="main-cards-card-location-content">${item.locale}</span>
@@ -46,9 +65,17 @@ let cards
   
     const modalAppear = document.querySelector('.overlay')
 const closeModal = document.querySelector('.modal-close')
+
+const modalLogo = document.querySelector('.modal-logo')
+const modalInfoPoster = document.querySelector('.modal-info-poster')
+
+
 modalAppear.style.display = "none"
 cards.forEach(element => {
  element.addEventListener('click', () => {
+    let poster = element.firstElementChild.src
+    modalLogo.src = poster
+    modalInfoPoster.src = poster
     modalAppear.style.display = "flex"
  })    
 })
@@ -76,10 +103,7 @@ searcInput.addEventListener("input", _.debounce(() => {
     searcPost()
 }, 500));
 
-
 // enter input - end
-
-
 
 // const cardSpace = document.querySelector('.main-cards')
 
